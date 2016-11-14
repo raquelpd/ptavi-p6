bfcd4adcff033620d8ccf70c6f555303d1295b92
Raquel Perez Dominguez <raquelpd@gamma37.aulas.gsyc.urjc.es>
2016-11-14 10:58:53 +0100

    Empezando

--- server.py
+++ server.py
@@ -5,6 +5,14 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+
+
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
+FICH = sys.argv[3]
+
+
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -25,7 +33,12 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 break
 
 if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        sys.exit('Usage: python3 server.py IP port audio_file')
+        
+  
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', (PORT), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
